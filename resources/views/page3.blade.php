c539b793f0819f18d5282dc7183778e0e57e74b2
Seu Nome <seuemail@example.com>
2024-12-21 20:25:03 -0300

    005

--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main_layout')
+
+@section('content')
     <h1>Welcome View and Blade!</h1>
     <h3>Page 3</h3>
     <h3>The value is {{ $value }} </h3>
@@ -14,5 +8,4 @@
         <h3>The value is <?= $value; ?> </h3>
         <h3>The value is <?php $value ?> </h3>
     -->
-</body>
-</html>
+@endsection
